c8560a4357499299f7b4dba21336f435246d8ec0
LiuQiDian <3230643253@qq.com>
2024-09-09 16:37:40 +0800

    update

--- examples/speed_ctrl/speed_ctrl.ino
+++ examples/speed_ctrl/speed_ctrl.ino
@@ -1,9 +1,35 @@
 #include "../../include/DJIMotorCtrlESP.hpp"
-M2006_P36 MOTOR(1);
-void setup() {
-  can_init(8, 18, 100);
 
+/* 速度控制 */
+// 3508电机
+M3508_P19 MOTOR(1);
+void setup()
+{
+  can_init(8, 18, 100);
 }
-void loop() {
-  MOTOR.set_location(8192*12);
+void loop()
+{
+  MOTOR.set_speed(50, 10);
+}
+
+// 2006电机
+M2006_P19 MOTOR(1);
+void setup()
+{
+  can_init(8, 18, 100);
+}
+void loop()
+{
+  MOTOR.set_speed(50, 10);
+}
+
+// GM6020电机
+GM6020 MOTOR(1);
+void setup()
+{
+  can_init(8, 18, 100);
+}
+void loop()
+{
+  MOTOR.set_speed(100, 10);
 }
